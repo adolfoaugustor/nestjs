99c291b8e7c6e871b64ebea909609217fcb373f0
AdolfoAugustoR <adolfoaugusto2010@hotmail.com>
2021-11-21 14:59:12 -0300

    adjustment

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(8000);
 }
 bootstrap();
